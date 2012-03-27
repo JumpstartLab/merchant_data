77290d6918d4b06f571aa3e2612485d859cb9a78
Matt Yoho <mby@mattyoho.com>
2012-03-27 08:01:48 -0400

    Helper functionality for evaluations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,24 @@
 class Customer < ActiveRecord::Base
 	has_many :invoices
+
+  def transactions
+    invoices.map(&:transactions).flatten
+  end
+  def favorite_merchant
+    merchant_transactions = transactions.group_by {|t| t.invoice.merchant_id }
+    merchant_with_most = merchant_transactions.sort_by {|m_id, txns| -txns.count}.first
+    Merchant.find_by_id(merchant_with_most.first)
+  end
+
+  def self.more_than_one
+    id = all.map(&:favorite_merchant_helper).compact.find{|m, c, id| c > 1}.last
+    find id
+  end
+
+  def favorite_merchant_helper
+    merchant_transactions = transactions.group_by {|t| t.invoice.merchant_id }
+    merchant_with_most = merchant_transactions.sort_by {|m_id, txns| -txns.count}.first
+    return nil unless merchant_with_most
+    [Merchant.find(merchant_with_most.first), merchant_with_most.last.count, id]
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,34 @@
 class Item < ActiveRecord::Base
   has_many :invoice_items
   belongs_to :merchant
+
+  def self.most_revenue(n)
+    all.sort.first(n)
+  end
+
+  def self.most_items(n)
+    all.sort_by {|i| -i.total_sold }.first(n)
+  end
+
+  def best_day
+    invoice_items.group_by {|ii| ii.updated_at.to_date}.sort_by {|date, items| -items.map(&:quantity).sum}.first
+  end
+
+  def total_revenue
+    total = invoice_items.inject(0) do |acc, ii|
+      acc + ii.quantity * ii.unit_price
+    end
+    BigDecimal.new(total)
+  end
+
+  def total_sold
+    total = invoice_items.inject(0) do |acc, ii|
+      acc + ii.quantity
+    end
+    BigDecimal.new(total)
+  end
+
+  def <=>(o)
+    (total_revenue <=> o.total_revenue) * -1
+  end
 end
